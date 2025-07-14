d8abd1448ac8de2b19f5f2a1984ac8dd7d3c3d17
Oleksandr-Berd <alex.berd86@gmail.com>
2025-07-14 07:24:06 +0300

    add sharedLyaout and the title comp

---
+++ src/components/Title/Title.tsx
@@ -0,0 +1,11 @@
+import * as SC from "./TitleStyled"
+
+const Title:React.FC = () => {
+    return ( 
+        <SC.TitleCon>
+            <h1>Our Pricing</h1>
+        </SC.TitleCon>
+     );
+}
+ 
+export default Title;
