2df481d92e49c4135841171b99cd754d527c480c
Oleksandr-Berd <alex.berd86@gmail.com>
2025-07-14 16:08:34 +0300

    logic is made

--- src/components/Console/ConsoleStyled.ts
+++ src/components/Console/ConsoleStyled.ts
@@ -1,33 +1,36 @@
 import styled from "@emotion/styled";
 
 import switchRight from "../../assets/images/switchRight.png"
+import switchLeft from "../../assets/images/switchLeft.png"
+import { YearType } from "@/utils/types";
 
-export const ConsoleCon = styled.div`
-display: flex;
-flex-direction: row;
-align-items: center;
-justify-content: center;
+export const ConsoleCon = styled.div<Partial<YearType>>`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
 
-margin-bottom: 80px;
+  margin-bottom: 80px;
 
-&>button{
+  & > button {
     width: 56px;
     height: 32px;
 
     margin-right: 24px;
 
-    background-image: url(${switchRight});
-}
+    background-image: ${({ isYear }) =>
+      isYear ? `url(${switchLeft})` : `url(${switchRight})`};
+  }
 
-& > span{
+  & > span {
     font-size: 15px;
     line-height: 1.9;
     font-weight: 700;
 
-    color: #6E728E;
+    color: #6e728e;
 
-    &:not(:last-of-type){
-        margin-right: 24px;
+    &:not(:last-of-type) {
+      margin-right: 24px;
     }
-}
-`
+  }
+`;
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,3 +1,8 @@
 export type ChildProps = {
     children: React.ReactNode;
 }
+
+export type YearType = {
+    isYear:boolean;
+    toggleYear:()=>void;
+}
